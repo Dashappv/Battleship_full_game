2b64d2e107f045f1b604dec3e0690bbaac9d9425
Dashappv <150603960+Dashappv@users.noreply.github.com>
2024-05-22 23:56:01 +0200

    Update new_full_game_final.py
    Comments are added

--- new_full_game_final.py
+++ new_full_game_final.py
@@ -8,10 +8,10 @@ for i in range(10):
         row.append(".")
     grid_user.append(row)
 
-# Printing the grid
 for row in grid_user:
     print(" ".join(map(str, row)))
 
+# Creating the grid for computer but without printing
 grid_computer = []
 for i in range(10):
     row = []
@@ -19,11 +19,12 @@ for i in range(10):
         row.append(".")
     grid_computer.append(row)
 
-
+# User allocates their ships
 def allocate_user_ships(grid_user, user_ships):
     print("Allocate ships for the user")
 
     for ship in range(5):
+        # Loop enables check if allocation of the ships is valid and within the grid
         while True: 
             x = int(input("Enter the x coordinate of the ship: "))
             y = int(input("Enter the y coordinate of the ship: "))
@@ -42,8 +43,8 @@ def allocate_user_ships(grid_user, user_ships):
     print(" ", " ".join(str(i) for i in range(len(grid_user))))
     for i, row in enumerate(grid_user):
         print(f"{i} {' '.join(str(cell) for cell in row)}")
-    #print(grid_user)
 
+# Random allocation of computer's ship 
 def allocate_computer_ships(grid_computer, computer_ships):
     print("Ships of computer were allocated.")
     computer_ships = []
@@ -52,38 +53,49 @@ def allocate_computer_ships(grid_computer, computer_ships):
         y = random.randint(0, 9)
         grid_computer[x][y] = "X"
         computer_ships.append((x, y))
+    # Following print is used to check if allocation happened correctly. To make computer's choice invisible, please outcomment the line below
     print(computer_ships)
 
+# User takes a turn to shoot 
 def user_turn(grid_computer, user_ships):
     print("User's turn to shoot!")
     print("Enter the coordinates of the cell you want to hit")
-    x = int(input("Enter the x coordinate: "))
-    y = int(input("Enter the y coordinate: "))
-    if grid_computer[x][y] == "X":
-        grid_computer[x][y] = "O"
-        print("Hit!")
-    else:
-        print("Miss!")
 
+    # Check if user shoot within the valid range of grid
+    while True:
+        x = int(input("Enter the x coordinate: "))
+        y = int(input("Enter the y coordinate: "))
+
+        if x < 0 or x > 9 or y < 0 or y > 9:
+            print("You are shooting outside of the grid. Try one more time")
+        elif grid_computer[x][y] == "X":
+            grid_computer[x][y] = "O"
+            print("Hit!")
+        else:
+            print("Miss!")
+            break
+
+# Computer takes turn to shoot
 def computer_turn(grid_user, computer_ships):
     print("Computer's turn to shoot!")
     x = random.randint(0, 9)
     y = random.randint(0, 9)
     if grid_user[x][y] == "X":
-        grid_user[x][y] = "O"
+        grid_user[x][y] = "O"                                            # shots of computer are displayed as "O", in case it hits user's ship
         print("Hit!")
+        
         print(" ", " ".join(str(i) for i in range(len(grid_user))))
     for i, row in enumerate(grid_user):
         print(f"{i} {' '.join(str(cell) for cell in row)}")
     else:
-        if grid_user[x][y] == ".":
-            grid_user[x][y] = "!"
+        if grid_user[x][y] == ".":                                   
+            grid_user[x][y] = "!"                                         # shots of computer are displayed as "!", in case it misses
             print("Miss!")
             print(" ", " ".join(str(i) for i in range(len(grid_user))))
     for i, row in enumerate(grid_user):
         print(f"{i} {' '.join(str(cell) for cell in row)}")
 
-
+# Initiation of the full game until game_over condition is reached
 def main():
     grid_user = [["." for _ in range(10)] for _ in range(10)]
     grid_computer = [["." for _ in range(10)] for _ in range(10)]
